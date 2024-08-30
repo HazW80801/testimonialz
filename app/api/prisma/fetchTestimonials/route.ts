ba33a6519f8c0a2ef632a6f78775bf2e4688c1c8
HazWahb8080 <hazem.wahb100@gmail.com>
2024-08-30 09:52:37 +0300

    fix

--- app/api/prisma/fetchTestimonials/route.ts
+++ app/api/prisma/fetchTestimonials/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import { prisma } from "@/prismaClient";
-import { Testimonial } from '@/types';
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);

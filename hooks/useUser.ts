9eef632f76af4493d1e71df9b83175c368b15a37
HazWahb8080 <hazem.wahb100@gmail.com>
2024-08-30 10:20:10 +0300

    router instead of redirect -- hooks/useUser.ts

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,14 +1,15 @@
 "use client"
 import { supabase } from "@/supabaseClient";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { User } from '@supabase/supabase-js';
 
 export default function useUser() {
     const [session, setSession] = useState<User | null>(null);
+    const router = useRouter()
     const fetchUser = async () => {
         const { data: { user } } = await supabase.auth.getUser()
-        if (!user){ redirect("/signin")}
+        if (!user) { router.push("/signin") }
         setSession(user)
     }
 

d3fb829d7bc12eff1be39fd888dd9c03cc22e4a0
HazWahb8080 <hazem.wahb100@gmail.com>
2024-08-30 11:58:10 +0300

    no export config in api route'

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -74,8 +74,3 @@ export async function POST(request: Request) {
         return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
     }
 }
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
